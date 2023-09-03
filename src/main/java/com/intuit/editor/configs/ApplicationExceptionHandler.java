4483736d44333ccff02e5d4bf50a3b38a0481397
B0221664 <yati.malik@airtel.com>
2023-09-03 23:44:25 +0530

    Logging

--- src/main/java/com/intuit/editor/configs/ApplicationExceptionHandler.java
+++ src/main/java/com/intuit/editor/configs/ApplicationExceptionHandler.java
@@ -3,6 +3,7 @@ package com.intuit.editor.configs;
 import com.intuit.editor.dto.Error;
 import com.intuit.editor.exception.ApplicationException;
 import com.intuit.editor.dto.Response;
+import com.intuit.editor.exception.ContentErrors;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -14,6 +15,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @ControllerAdvice
 public class ApplicationExceptionHandler {
+
+    @ResponseBody
+    @ExceptionHandler(value = {Exception.class})
+    public ResponseEntity<Response<Object>> handleException(Exception exception) {
+        return new ResponseEntity<>(Response.failureResponseWithBody(new Error(ContentErrors.SOMETHING_WENT_WRONG.getCode(), ContentErrors.SOMETHING_WENT_WRONG.getMessage())), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
     @ResponseBody
     @ExceptionHandler(value = {ApplicationException.class})
     public ResponseEntity<Response<Object>> handleException(ApplicationException exception) {
--- src/main/java/com/intuit/editor/rest/EditorController.java
+++ src/main/java/com/intuit/editor/rest/EditorController.java
@@ -4,6 +4,7 @@ import com.intuit.editor.dto.Response;
 import com.intuit.editor.request.CreateContentModel;
 import com.intuit.editor.request.EditorContent;
 import com.intuit.editor.services.IEditorService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 public class EditorController {
 
@@ -19,27 +21,32 @@ public class EditorController {
 
     @GetMapping("/getContent")
     public ResponseEntity<Response<EditorContent>> getContent(@RequestParam String id){
+        log.info("[getContent] request received for id: "+ id);
         return new ResponseEntity<>(Response.successResponse(this.editorService.getContent(id)), HttpStatus.OK);
     }
 
     @PostMapping("/createContent")
     public ResponseEntity<Response<Object>> createContent(@RequestBody CreateContentModel editorContent){
+        log.info("[createContent] request received for title: "+ editorContent.getTitle());
         return new ResponseEntity<>(Response.successResponse(this.editorService.createContent(editorContent)), HttpStatus.OK);
     }
 
     @PostMapping("/updateContent")
     public ResponseEntity<Response<Object>> updateContent(@RequestBody EditorContent editorContent){
+        log.info("[updateContent] request received for id: "+ editorContent.getContentId());
         this.editorService.updateContent(editorContent);
         return new ResponseEntity<>(Response.successResponse(), HttpStatus.OK);
     }
 
     @PostMapping("/getResolvedContent")
     public ResponseEntity<Response<EditorContent>> getResolvedContent(@RequestBody EditorContent editorContent){
+        log.info("[getResolvedContent] request received for id: "+ editorContent.getContentId());
         return new ResponseEntity<>(Response.successResponse(this.editorService.getResolvedContent(editorContent)), HttpStatus.OK);
     }
 
     @GetMapping("/getContentEntries")
     public ResponseEntity<Response<List<EditorContent>>> getContentEntries(){
+        log.info("[getContentEntries] request received");
         return new ResponseEntity<>(Response.successResponse(this.editorService.getContentEntries()), HttpStatus.OK);
     }
 
